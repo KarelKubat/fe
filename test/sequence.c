08f40f4ae64a25fd8dcbc69f69566f08ea2e33b4
Karel Kubat <karel@kubat.nl>
2015-03-19 10:27:32 +0100

    wip

--- test/sequence.c
+++ test/sequence.c
@@ -7,9 +7,11 @@ int main() {
 
     ctx.seed = "secret";
     ctx.msg_verbosity = 1;
-    
-    for (i = 0; i <= 10000; i++)
+
+    printf("Generating 10000 random bytes... ");
+    for (i = 0; i < 10000; i++)
 	s[i] = fe_randbyte(&ctx, i, hashval);
+
     printf("Sequence generated, first 100 values:\n");
     for (i = 0; i < 100; i++)
 	printf("%d ", s[i]);
@@ -19,7 +21,7 @@ int main() {
 	if (s[i] == s[i + 1] && s[1] == s[i + 2])
 	    fe_error("Triplicate value at index %d\n", i);
 
-    for (i = 10000; i >= 0; i--) {
+    for (i = 10000 - 1; i >= 0; i--) {
 	r = fe_randbyte(&ctx, i, hashval);
 	if (s[i] != r)
 	    fe_error("Sequence test failed at index %d\n", i);
