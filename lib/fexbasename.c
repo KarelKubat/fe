eb5c0b3ecc172188a90cbd2db876ebe0db679c5f
Karel Kubat <karel@kubat.nl>
2017-11-24 22:03:33 +0100

    fixed for MacOSX

--- lib/fexbasename.c
+++ lib/fexbasename.c
@@ -3,5 +3,10 @@
 char *fe_xbasename(char const *path) {
   /* Strdup the path, basename chops it up. The caller must free the return
    * value. */
-  return basename(fe_xstrdup(path));
+    char *copy = fe_xstrdup(path);
+    char *base = basename(copy);
+    char *ret  = fe_xstrdup(base);
+
+    free(copy);
+    return ret;
 }
--- lib/fexdirname.c
+++ lib/fexdirname.c
@@ -3,5 +3,10 @@
 char *fe_xdirname(char const *path) {
     /* Strdup the path, dirname chops it up. The caller is responsible for
      * freeing the return value. */
-    return dirname(fe_xstrdup(path));
+    char *copy = fe_xstrdup(path);
+    char *dirn = dirname(copy);
+    char *ret  = fe_xstrdup(dirn);
+    
+    free(copy);
+    return ret;
 }
