a3706f85a2c1e5c5452da04e3e12e223b9269768
Karel Kubat <karel@kubat.nl>
2018-12-12 15:51:43 +0100

    added openat call

--- lib/libc/openat.c
+++ lib/libc/openat.c
@@ -1,7 +1,26 @@
 #include "../../fe.h"
 
-int openat(int fd, char const *path, int oflag, ...) {
-    fe_msg(fectx(), "Request to openat(%d, %s, %d, ...\n", fd, path, oflag);    
-    fe_error("fe " VER ": cannot handle openat, aborting\n");
-    return -1;
+int openat(int f, char const *path, int oflag, ...) {
+    static int (*real_openat)(int fd, char const *path, int oflag, ...);
+    int mode, fd;
+    va_list args;
+
+    /* Get mode arg. According to the manpage this is only required during
+     * creation, but we pass it anyway. Let the real openat() figure out if
+     * it is needed. */
+    va_start(args, oflag);
+    mode = va_arg(args, int);
+    va_end(args);
+    
+    /* Get hook to real openat */
+    if (!real_openat)
+	real_openat = fe_dllookup("openat");
+
+    /* Perform the open */
+    fd = real_openat(f, path, oflag, mode);
+    fe_msg(fectx(), "Request to openat(%d, %s, %d, ...) => %d\n",
+	   f, path, oflag, fd);
+    fe_target_by_path(path, fd);
+
+    return fd;
 }
